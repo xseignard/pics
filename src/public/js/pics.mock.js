7feab886e646b5889f95e68628633e69e960abd6
xseignard <xavier.seignard@gmail.com>
2016-02-23 00:42:49 +0100

    Created mock library

--- src/public/js/pics.mock.js
+++ src/public/js/pics.mock.js
@@ -1,11 +1,9 @@
-(function(io){
+(function(){
 
 	var Pics = function(host, handleWinner) {
-		this.socket = io.connect(host);
 		this.handleWinner = handleWinner;
 		var _self = this;
-		this.socket.on('result', function(result) {
-			console.log(result);
+		this.mockResult = function(result) {
 			if (result.id === '1.1' && !_self.result1) {
 				_self.result1 = result;
 			}
@@ -15,13 +13,12 @@
 			if (_self.result1 && _self.result2) {
 				_self.handleWinner(_self.result1, _self.result2);
 			}
-		});
+		};
 	};
 
 	Pics.prototype.start = function(hexColor, duration) {
 		if (this.result1) delete this.result1;
 		if (this.result2) delete this.result2;
-		this.socket.emit('start', hexColor);
 		var _self = this;
 		setTimeout(function() {
 			_self.end();
@@ -29,21 +26,33 @@
 	};
 
 	Pics.prototype.end = function() {
-		this.socket.emit('take_picture');
+		var _self = this;
+		var getRandomInt = function(min, max) {
+			return Math.floor(Math.random() * (max - min)) + min;
+		};
+		var duration1 = getRandomInt(3000, 6000);
+		var duration2 = getRandomInt(3000, 6000);
+		setTimeout(function() {
+			_self.mockResult({ id: '1.1', delta: Math.random() })
+		}, duration1);
+		setTimeout(function() {
+			_self.mockResult({ id: '1.2', delta: Math.random() })
+		}, duration2);
 	};
 
 	Pics.prototype.win = function(deviceId) {
-		this.socket.emit('led', { deviceId: deviceId, on: true });
+		console.log('winner ' + deviceId + ' will turn its leds on');
 	};
 
 	Pics.prototype.lose = function(deviceId) {
-		this.socket.emit('led', { deviceId: deviceId, on: false });
+		console.log('loser ' + deviceId + ' will turn its leds off');
 	};
 
 	Pics.prototype.led = function(deviceId, on) {
-		this.socket.emit('led', { deviceId: deviceId, on: on });
+		var onOff = on ? 'on' : 'off';
+		console.log('leds of ' + deviceId + ' will be turned ' + onOff );
 	};
 
 	window.Pics = Pics;
 
-})(io);
+})();
